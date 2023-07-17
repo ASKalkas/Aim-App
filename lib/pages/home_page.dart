73d672893b3a5871bfa7c2f7dba4e2fc46effb30
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2023-07-17 13:28:42 +0300

    Added homepage and assets

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: FractionalOffset(0.5, 0.8),
+                child: SvgPicture.asset(
+                  "assets/images/Logo.svg",
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Align(
+                  alignment: FractionalOffset(0.5, 0.5),
+                  child: SvgPicture.asset(
+                    'assets/images/Vector.svg',
+                    semanticsLabel: 'Loading SVG',
+                    width: 50,
+                    height: 50,
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
